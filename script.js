e682391d4c147327c064a876b4922aab8bfd8289
commandblocks0 <153120669+commandblocks0@users.noreply.github.com>
2025-03-11 11:35:31 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -81,6 +81,7 @@ function updateDisplay() {
     const itemTable = document.getElementById('itemTable')
     itemTable.innerHTML = `
         <tr>
+            <th>Day</th>
             <th>Name</th>
             <th>Price</th>
             <th>Info</th>
@@ -88,14 +89,17 @@ function updateDisplay() {
     `
     items.forEach(item => {
         const row = document.createElement('tr')
+        const day = document.createElement('td')
         const name = document.createElement('td')
         const price = document.createElement('td')
         const info = document.createElement('td')
 
+        day.innerText = item.day || 1
         name.innerText = item.name
         price.innerText = item.price.toLocaleString()
         info.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>'
 
+        row.appendChild(day)
         row.appendChild(name)
         row.appendChild(price)
         row.appendChild(info)
@@ -158,7 +162,8 @@ document.getElementById("itemPurchaseButton").addEventListener('click', () => {
         name: itemName.value,
         price: parseInt(itemPrice.value),
         img: itemImg.value || null,
-        url: itemUrl.value || null
+        url: itemUrl.value || null,
+        day: data.day
     })
 
     itemName.value = ''
